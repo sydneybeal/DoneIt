a1c4314642c49fc297ac2fa9a86c462259af5db9
Sydney Beal <sydneybeal@miami.edu>
2018-09-18 22:02:27 -0500

    Make data persistent using user defaults

--- Done It/TodoListViewController.swift
+++ Done It/TodoListViewController.swift
@@ -12,9 +12,15 @@ class TodoListViewController: UITableViewController {
 
     var itemArray = ["Book flight", "Schedule haircut", "Complete todo app"]
     
+    var defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
     }
     
     // MARK: - TableView datasource methods
@@ -60,6 +66,8 @@ class TodoListViewController: UITableViewController {
             // callback (in the future) after the below has completed
             print("Success!")
             self.itemArray.append(textField.text!)
+            //save item array to user defaults
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             self.tableView.reloadData()
         }
         
