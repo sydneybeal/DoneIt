e9ef9291d9cf414ffb532dd051348e2572eeeace
Sydney Beal <sydneybeal@miami.edu>
2018-09-18 20:43:09 -0500

    Initial Commit

---
+++ Done It/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Done It
+//
+//  Created by Sydney Beal on 9/18/18.
+//  Copyright © 2018 Sydney Beal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
